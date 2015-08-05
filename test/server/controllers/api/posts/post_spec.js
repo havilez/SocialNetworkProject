c83e1bffc01a15b08c66c05dc97489880451bad4
HAROLD <havilez@gmail.com>
2015-08-04 23:04:01 -0400

    added GET /api/posts test with db

--- test/server/controllers/api/posts/post_spec.js
+++ test/server/controllers/api/posts/post_spec.js
@@ -5,22 +5,52 @@ var should = require('should');
 
 var api = require('../../../support/api');
 
+var Post = require('../../../../../models/posts');
+
 
 
 var ctrl = require('../../../../../controllers/api/posts');
 
 describe('controllers.api.posts', function () {
 
+    // clear out data for tests
+    beforeEach(function (done) {
+        Post.remove({}, done);
+    });
+
+
+
     it('Posts controller exists', function () {
         expect(ctrl).to.exist;
     });
 
     describe('GET /api/posts', function () {
+
+        // add three records
+        beforeEach(function (done) {
+            var posts = [
+                {body: 'post1', username: 'dickeyxxx'},
+                {body: 'post2', username: 'dickeyxxx' },
+                {body: 'post3' , username: 'dickeyxxx'}
+            ]
+
+            Post.create(posts,done);
+        });
+
         it('exists', function (done) {
             api.get('/api/posts')
                 .expect(200)
                .end(done)
-        })
+        });
+
+        it('has 3 posts', function (done) {
+            api.get('/api/posts')
+                .expect(200)
+                .expect(function (response) {
+                    expect(response.body).to.have.length(3);
+                })
+                .end(done)
+        });
     });
 
 });
